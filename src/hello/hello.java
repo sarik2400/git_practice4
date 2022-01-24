679fa246ca3dba5df02bc32f4592c0f7bed25ff7
srd <sarik2400@gmail.com>
2022-01-24 10:39:41 -0500

    doing final work on hello

--- src/hello/hello.java
+++ src/hello/hello.java
@@ -3,5 +3,6 @@ package hello;
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("B25 Learning git and Github");
     }
 }
